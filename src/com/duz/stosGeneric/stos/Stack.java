c9353127c1dfc222913ee64b8f9cb8cbae21c1e7
monaduz <duzmonika@gmail.com>
2018-06-30 11:26:16 +0200

    typy generyczne

---
+++ src/com/duz/stosGeneric/stos/Stack.java
@@ -0,0 +1,51 @@
+package com.duz.stosGeneric.stos;
+
+import java.util.EmptyStackException;
+
+public class Stack {
+
+    private Boxx[] stack;
+    private Boxx top;
+    private Integer size;
+
+    public Stack(Integer size) {
+        stack = new Boxx[size];
+        top = null;
+        this.size = 0;
+    }
+
+    public void push(Boxx element) {
+        if (size != stack.length) {
+            stack[size++] = element;
+            top = element;
+        } else System.out.println( "stos pełny" );
+    }
+
+    public Boxx peek() {
+        if (!isEmpty()) {
+            return top;
+        } else {
+            throw new EmptyStackException();
+        }
+    }
+
+    public Boxx pop() {
+        if (!isEmpty()) {
+            size--;
+            if (size==0){
+                top = null;
+              return stack[size];
+            }
+            top = stack[size-1];
+            return stack[size];
+        }
+        else{
+            throw new EmptyStackException();
+        }
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+}
