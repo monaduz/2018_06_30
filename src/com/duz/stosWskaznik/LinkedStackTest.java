9806ee9178ae8028a630fce092b3d835be55f712
monaduz <duzmonika@gmail.com>
2018-06-30 13:20:32 +0200

    LinkedStack

--- src/com/duz/stosWskaznik/LinkedStackTest.java
+++ src/com/duz/stosWskaznik/LinkedStackTest.java
@@ -5,21 +5,22 @@ public class LinkedStackTest {
     public static void main(String[] args) {
         Boxx<Integer> boxx1 = new Boxx<>( 23 );
         Boxx<String> boxx2 = new Boxx<>( "ggfdgdf" );
-        Boxx<Double> boxx3 = new Boxx<>( 23.25);
+        Boxx<Double> boxx3 = new Boxx<>( 23.25 );
 
         LinkedStack linkedStack = new LinkedStack();
 
-        System.out.println("isEmpty? " + linkedStack.isEmpty() );
+        System.out.println( "isEmpty? " + linkedStack.isEmpty() );
         linkedStack.push( boxx1 );
-        System.out.println("isEmpty? " +  linkedStack.isEmpty() );
-        System.out.println("wydrukuj stos: \n" + linkedStack);
-        linkedStack.push(boxx2);
-        System.out.println("wydrukuj stos: \n" + linkedStack);;
+        System.out.println( "isEmpty? " + linkedStack.isEmpty() );
+        System.out.println( "wydrukuj stos: \n" + linkedStack );
+        linkedStack.push( boxx2 );
+        System.out.println( "wydrukuj stos: \n" + linkedStack );
+        ;
         linkedStack.push( boxx3 );
-        System.out.println("wierzchołek: " + linkedStack.peek() );
+        System.out.println( "wierzchołek: " + linkedStack.peek() );
         linkedStack.pop();
         linkedStack.pop();
-        System.out.println("wydrukuj stos: \n" + linkedStack);
+        System.out.println( "wydrukuj stos: \n" + linkedStack );
 
 
     }
